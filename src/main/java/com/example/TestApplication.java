79e06abbb95524880496053170cd42c8272cf123
ckh <ckh520_haohao@163.com>
2020-03-03 20:16:27 +0800

    A master

--- src/main/java/com/example/TestApplication.java
+++ src/main/java/com/example/TestApplication.java
@@ -3,7 +3,7 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//dev
+//master
 @SpringBootApplication
 public class TestApplication {
 
